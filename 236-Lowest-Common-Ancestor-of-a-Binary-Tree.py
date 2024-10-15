8a34043429defd846760854855d126241f1a4149
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2024-10-15 00:39:21 -0400

    [236] [Time Beats: 83.03%] [Memory Beats: 14.37%] - LeetPush

---
+++ 236-Lowest-Common-Ancestor-of-a-Binary-Tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # if root not found
+        if not root:
+            return None
+        # found one of the nodes
+        elif root == p or root == q:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        # if found both, return root
+        if left and right:
+            return root
+        # else return the root that contains one of the nodes found
+        return left or right
