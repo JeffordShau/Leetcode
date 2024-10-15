f183099e7e315db1cb844df8aab78a7b2e85f1fb
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2024-10-15 15:38:14 -0400

    [1448] [Time Beats: 90.26%] [Memory Beats: 45.21%] - LeetPush

--- 1448-Count-Good-Nodes-in-Binary-Tree.py
+++ 1448-Count-Good-Nodes-in-Binary-Tree.py
@@ -6,18 +6,12 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        self.counter = 1
 
-        def dfs(root: TreeNode, maxVal: int) -> None:
+        def dfs(root: TreeNode, maxVal: int) -> int:
             if not root: 
-                return
+                return 0
             elif root.val >= maxVal:
-                self.counter += 1
-            maxVal = max(root.val, maxVal)
-            dfs(root.left, maxVal)
-            dfs(root.right, maxVal)
-        
-        dfs(root.left, root.val)
-        dfs(root.right, root.val)
-
-        return self.counter
+                maxVal = root.val
+                return 1 + dfs(root.left, maxVal) + dfs(root.right, maxVal)
+            return dfs(root.left, maxVal) + dfs(root.right, maxVal)
+        return dfs(root, root.val)
