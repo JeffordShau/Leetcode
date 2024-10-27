768c85d96629308e7ebc9f96ff0f4e5eb63b0309
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2024-10-27 03:48:55 -0400

    [105] [Time Beats: 62.03%] [Memory Beats: 61.42%] - LeetPush

--- 105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal.py
+++ 105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal.py
@@ -6,25 +6,23 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        def helper(preorder, segment: List[int]) -> Optional[TreeNode]:
-            # print(segment)
-            if not segment or not preorder:
-                return None
-            rootIdx = -1
-            val = 0
-            for num in preorder:
-                if num in segment:
-                    # print(num)
-                    rootIdx = segment.index(num)
-                    val = num
-                    break
-            # print(\rootIdx\, rootIdx)
-            root = TreeNode(num)
-            preorder.remove(val)
-            root.left = helper(preorder, segment[:rootIdx]) if rootIdx != 0 else None
-            root.right = helper(preorder, segment[rootIdx + 1:]) if rootIdx != len(segment) else None
-            return root
-        return helper(preorder, inorder)
+        # print(segment)
+        if not inorder or not preorder:
+            return None
+        rootIdx = -1
+        val = 0
+        for num in preorder:
+            if num in inorder:
+                # print(num)
+                rootIdx = inorder.index(num)
+                val = num
+                break
+        # print(\rootIdx\, rootIdx)
+        root = TreeNode(num)
+        preorder.remove(val)
+        root.left = self.buildTree(preorder, inorder[:rootIdx]) if rootIdx != 0 else None
+        root.right = self.buildTree(preorder, inorder[rootIdx + 1:]) if rootIdx != len(inorder) else None
+        return root
         
         
 
