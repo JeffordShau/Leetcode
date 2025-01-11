88c66f1e98d4f545548157fad32b037e393d0dd2
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2025-01-11 02:47:26 -0500

    [347] [Time Beats: 91.99%] [Memory Beats: 39.63%] - LeetPush

--- 347-Top-K-Frequent-Elements.py
+++ 347-Top-K-Frequent-Elements.py
@@ -4,11 +4,27 @@ class Solution:
         for num in nums:
             counter[num] += 1
 
-        maxHeap = [(-freq, key) for key, freq in counter.items()]
-        heapq.heapify(maxHeap)
+        nums = list(set(nums))
 
-        res = []
-        for i in range(k):
-            res.append(heapq.heappop(maxHeap)[1])
+        k = len(nums) - k
 
-        return res
+        def quicksort(l, r):
+            p = l
+            for i in range(l, r):
+                if counter[nums[i]] <= counter[nums[r]]:
+                    nums[p], nums[i] = nums[i], nums[p]
+                    p += 1
+            nums[p], nums[r] = nums[r], nums[p]
+
+            if p == k:
+                print(nums)
+                res = []
+                for i in range(k, len(nums)):
+                    res.append(nums[i])
+                return res
+            elif p < k:
+                return quicksort(p + 1, r)
+            else:
+                return quicksort(l, p - 1)
+        
+        return quicksort(0, len(nums) - 1)
