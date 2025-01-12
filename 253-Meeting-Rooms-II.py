10248136520038860631fc51f3693c82f70a01a6
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2025-01-12 15:39:02 -0500

    [253] [Time Beats: 86.33%] [Memory Beats: 29.99%] - LeetPush

--- 253-Meeting-Rooms-II.py
+++ 253-Meeting-Rooms-II.py
@@ -1,19 +1,21 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        intervals.sort()
-        minHeap = []
-        heapq.heapify(minHeap)
-        res = 0
+        if not intervals:
+            return 0
 
-        for i_start, i_end in intervals:
-            if len(minHeap) == 0:
-                heapq.heappush(minHeap, i_end)
-            else:
-                curr_end = minHeap[0]
-                if curr_end > i_start:
-                    heapq.heappush(minHeap, i_end)
-                else:
-                    heapq.heappushpop(minHeap, i_end)
-            res = max(res, len(minHeap))
+        usedRooms = 0
+
+        starts = sorted(i[0] for i in intervals)
+        ends = sorted(i[1] for i in intervals)
+
+        endPtr = 0
+        startPtr = 0
+
+        while startPtr < len(intervals):
+            if starts[startPtr] >= ends[endPtr]:
+                usedRooms -= 1
+                endPtr += 1
+            usedRooms += 1
+            startPtr += 1
         
-        return res
+        return usedRooms
